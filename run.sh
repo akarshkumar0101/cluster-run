53705be6c82c9bc238499e98a5ce79212a3b863f
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-03-04 14:42:24 -0500

    sync

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 
 
-sleep 04 && python train.py --level 0
-sleep 04 && python train.py --level 1
-sleep 04 && python train.py --level 2
-sleep 04 && python train.py --level 3
+# sleep 04 && python train.py --level 0
+# sleep 04 && python train.py --level 1
+# sleep 04 && python train.py --level 2
+# sleep 04 && python train.py --level 3
+
+echo $0
+echo $0
+echo $0
 
 
 
