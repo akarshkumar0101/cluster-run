20c760207c7bb32ec8bc4c524ce43eadb7b53dd9
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-03-04 01:59:53 -0500

    sync

--- run_cluster.py
+++ run_cluster.py
@@ -61,5 +61,7 @@ while True:
         break
 
     command = f'python run_node.py {args.dir_run} --dir {args.dir} --mem_gpu {args.mem_gpu} --mem_cpu {args.mem_cpu}'
-    run(command.split())
+    command = f'ssh visiongpu47.csail.mit.edu \"{command}\"'
+    print(command)
+    run(command, shell=True)
     time.sleep(.1)
