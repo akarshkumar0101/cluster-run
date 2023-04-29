a1721e228033886009c4fcdee2fd7f57b48a4548
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-04-29 14:50:01 -0400

    check which script

---
+++ check_which.sh
@@ -0,0 +1,5 @@
+
+echo "done1" && which python pip jupyter cmake
+echo "done2" && which python pip jupyter cmake
+echo "done3" && which python pip jupyter cmake
+echo "done4" && which python pip jupyter cmake
