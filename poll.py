501441749568061af9cb172a447d7dc124601584
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-04-29 14:47:27 -0400

    add conda env flag

--- poll.py
+++ poll.py
@@ -2,10 +2,6 @@ import argparse
 # import json
 # import logging
 import os
-import re
-import subprocess
-import sys
-from datetime import datetime
 
 import numpy as np
 
@@ -15,13 +11,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--servers', nargs='*', type=str, default=None,
                     help='either a list of servers or a file containing a list of servers')
 parser.add_argument('--mem-gpu', type=int, default=5000, help='gpu memory needed for each job (in MB)')
+
+
 # parser.add_argument('--mem-cpu', type=int, default=5000, help='cpu memory needed for each job (in MB)')
 
 def main(args):
     print(args)
 
     servers = args.servers
-    if len(args.servers)==1 and os.path.isfile(args.servers[0]):
+    if len(args.servers) == 1 and os.path.isfile(args.servers[0]):
         servers = read_clean(args.servers[0])
     print('----------------------')
     print(f'Found {len(servers):05d} servers: ')
@@ -35,11 +33,11 @@ def main(args):
         if gpu_stats is None:
             print('Server not responding/other nvidia-smi error, skipping...')
             continue
-        
+
         print([f'{f}/{t}' for f, t in zip(gpu_stats['free'], gpu_stats['total'])])
-        n_commands += np.sum(gpu_stats['free']//args.mem_gpu)
-        print(f"   GPU possible jobs: {gpu_stats['free']//args.mem_gpu}")
-        print(f"Server possible jobs: {np.sum(gpu_stats['free']//args.mem_gpu)}")
+        n_commands += np.sum(gpu_stats['free'] // args.mem_gpu)
+        print(f"   GPU possible jobs: {gpu_stats['free'] // args.mem_gpu}")
+        print(f"Server possible jobs: {np.sum(gpu_stats['free'] // args.mem_gpu)}")
 
         # print(f"Server GPU stats: {' '.join([f'{k}: {v[i_gpu]:06d} MB' for k, v in gpu_stats.items()])}")
         print(f"Required memory/command: {args.mem_gpu} MB")
@@ -48,7 +46,6 @@ def main(args):
     print('----------------------')
     print(f'Total possible jobs: {n_commands}')
 
-if __name__=='__main__':
-    args = parser.parse_args()
-    main(args)
 
+if __name__ == '__main__':
+    main(parser.parse_args())
