e8663ad2a9c2d7a90addcf46d2467ed9447bd5e4
Akarsh Kumar <akarshkumar0101@gmail.com>
2024-02-28 17:34:05 -0500

    sync

---
+++ temp.py
@@ -0,0 +1,6 @@
+import time
+import os
+print("Starting")
+print(os.getenv('CUDA_VISIBLE_DEVICES'))
+time.sleep(10)
+print("Ending")
---
+++ temp.sh
@@ -0,0 +1,7 @@
+echo "a"
+python fjewiaofjeiowafwe.py
+echo "b"
+sleep 3
+echo "a"
+python fjewiaofjeiowafwe.py
+echo "b"
